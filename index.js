c6e87f4a032cc8b1734a95ed10a60a3f4cc96814
sluchznak <sluchainiyznak@gmail.com>
2016-02-17 13:16:33 +0600

    removed environment

--- index.js
+++ index.js
@@ -5,13 +5,7 @@ module.exports = {
   name: 'ember-techan',
   included: function(app) {
     this._super.included(app);
-    app.import({
-      development: app.bowerDirectory + '/d3/d3.js',
-      production: app.bowerDirectory + '/d3/d3.min.js'
-    });
-    app.import({
-      development: app.bowerDirectory + '/techan/dist/techan.js',
-      production: app.bowerDirectory + '/techan/dist/techan.min.js'
-    });
+    app.import(app.bowerDirectory + '/d3/d3.min.js');
+    app.import(app.bowerDirectory + '/techan/dist/techan.min.js');
   }
 };
