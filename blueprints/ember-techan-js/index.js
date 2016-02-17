a64fb0c17c9e5360df43ec7371d7ebaadb72c15f
sluchznak <sluchainiyznak@gmail.com>
2016-02-17 11:57:05 +0600

    added blueprints

---
+++ blueprints/ember-techan-js/index.js
@@ -0,0 +1,11 @@
+/* jshint node: true */
+'use strict';
+
+module.exports = {
+  name: 'ember-techan-js',
+  included: function(app) {
+    this._super.included(app);
+    app.import(app.bowerDirectory + '/d3/d3.min.js');
+    app.import(app.bowerDirectory + '/techan/dist/techan.min.js');
+  }
+};
--- index.js
+++ index.js
@@ -2,16 +2,5 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-techan-js',
-  included: function(app) {
-    this._super.included(app);
-    app.import({
-      development: app.bowerDirectory + '/d3/d3.js',
-      production: app.bowerDirectory + '/d3/d3.min.js'
-    });
-    app.import({
-      development: app.bowerDirectory + '/techan/dist/techan.js',
-      production: app.bowerDirectory + '/techan/dist/techan.min.js'
-    });
-  }
+  name: 'ember-techan-js'
 };
