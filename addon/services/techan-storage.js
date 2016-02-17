43ea217bacd2a30ef2d78e8be17b964110300faf
sluchznak <sluchainiyznak@gmail.com>
2016-02-17 21:57:57 +0600

    fix data overflow

--- addon/services/techan-storage.js
+++ addon/services/techan-storage.js
@@ -4,8 +4,12 @@ export default Ember.Service.extend(Ember.Evented, {
   data: [],
   addItem(item) {
     let data = this.get('data');
+    if (data.length > 250) {
+      data.shift();
+    }
     data.push(item);
     this.set('data', data);
+    console.log(data.length);
     this.trigger('newItem');
   }
 });
