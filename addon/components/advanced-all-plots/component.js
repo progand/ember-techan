8bcd7eb93382c2d458a11c74080d50242a3549ac
progand <lach@ukr.net>
2016-03-07 14:18:26 +0200

    Update component.js
    Redraw chart on window resize

--- addon/components/advanced-all-plots/component.js
+++ addon/components/advanced-all-plots/component.js
@@ -108,6 +108,20 @@ export default Ember.Component.extend({
 
     this.set('svg', svg);
     this.draw();
+    
+    this.updatePlotWidth = () => {
+      Ember.run.bind(this, Ember.run.throttle(this, function () {
+        this.set('width', this.$().width());
+        this.actions.redraw.apply(this, ...arguments);
+      }, 150));
+    }
+    Ember.$(window).on('resize', this.updatePlotWidth);
+    this.updatePlotWidth();
+  },
+  willDestroyElement() {
+    this._super(...arguments);
+    Ember.$(window).off('resize', this.updatePlotWidth);
+    this.updatePlotWidth = null;
   },
   isIndicatorEnable(indicatorName) {
     return $.inArray(indicatorName, this.get('enabledIndicators')) !== -1;
